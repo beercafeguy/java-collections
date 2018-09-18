cfc5128acf3b603cf62c49ee685043263837585f
Hem Chandra <hemchandra@outlook.com>
2018-09-18 18:50:22 +0530

    Another way to compare is using comparator

--- src/main/java/com/beercafeguy/java/compare/EmployeeApp.java
+++ src/main/java/com/beercafeguy/java/compare/EmployeeApp.java
@@ -2,6 +2,7 @@ package com.beercafeguy.java.compare;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class EmployeeApp {
@@ -10,10 +11,26 @@ public class EmployeeApp {
         eList.add(new Employee(101,"Hem",2009.0));
         eList.add(new Employee(102,"Abhay",2011.9));
         eList.add(new Employee(103,"Santry",2000.6));
+
+        System.out.println("Sorting using comparable (Compare using salary)");
         Collections.sort(eList);
         for (Employee e:eList
              ) {
             System.out.println(e);
         }
+
+        System.out.println("Sorting using Comparator (Compare using names)");
+        Collections.sort(eList, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (Employee e:eList
+                ) {
+            System.out.println(e);
+        }
+
     }
 }
